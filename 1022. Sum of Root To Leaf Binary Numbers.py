7d60cb3a69113cb033b065647878d11d6e8c937d
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-02-24 11:17:48 +0530

    Add files via upload

---
+++ 1022. Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, current_sum):
+            if not node:
+                return 0
+            
+            current_sum = (current_sum << 1) | node.val
+            
+            if not node.left and not node.right:
+                return current_sum
+                
+            return dfs(node.left, current_sum) + dfs(node.right, current_sum)
+            
+        return dfs(root, 0)
