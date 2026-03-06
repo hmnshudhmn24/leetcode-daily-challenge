62e7709090cc5109dfa9d1d5204dd8fef7377cbe
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-03-06 11:20:41 +0530

    Add files via upload

---
+++ 1656. Design an Ordered Stream.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class OrderedStream:
+    def __init__(self, n: int):
+        self.stream = [None] * n
+        self.ptr = 0
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey - 1] = value
+        res = []
+        while self.ptr < len(self.stream) and self.stream[self.ptr] is not None:
+            res.append(self.stream[self.ptr])
+            self.ptr += 1
+        return res
