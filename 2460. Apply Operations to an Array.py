faf641ed96bfbd27a7ca03401f9ae64324dfc0b0
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-03-24 08:12:39 +0530

    Add files via upload

---
+++ 2460. Apply Operations to an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def applyOperations(self, nums: list[int]) -> list[int]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1] and nums[i] != 0:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        res = [x for x in nums if x != 0]
+        return res + [0] * (len(nums) - len(res))
