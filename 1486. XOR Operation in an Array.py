5ad004e34f9a7b1b5e1c29388c6609f4d6bda3dc
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-03-26 17:52:44 +0530

    Add files via upload

--- 1486. XOR Operation in an Array.py
+++ 1486. XOR Operation in an Array.py
@@ -2,5 +2,5 @@ class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         res = 0
         for i in range(n):
-            res ^= start + 2 * i
+            res ^= (start + 2 * i)
         return res
