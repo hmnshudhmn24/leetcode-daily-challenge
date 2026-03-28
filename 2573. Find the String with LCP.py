77ab4bc89e840dd2929ee0bcd85ad2ef6f2706fd
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-03-28 10:30:07 +0530

    Add files via upload

--- 2573. Find the String with LCP.py
+++ 2573. Find the String with LCP.py
@@ -1,40 +1,28 @@
 class Solution:
-    def findTheString(self, lcp: List[List[int]]) -> str:
+    def findTheString(self, lcp: list[list[int]]) -> str:
         n = len(lcp)
-        s = [""] * n
-        current_char = 'a'
-        
-        # Phase 1: Construct the string greedily
-        for i in range(n):
-            if s[i]: continue # Already assigned
-            
-            if current_char > 'z':
-                return "" # Ran out of characters
-                
-            s[i] = current_char
-            for j in range(i + 1, n):
-                if lcp[i][j] > 0:
-                    s[j] = current_char
-            
-            current_char = chr(ord(current_char) + 1)
-            
-        # Check if any position was left unassigned (rare logic catch)
-        if any(c == "" for c in s):
-            return ""
+        res = [0] * n
+        char_val = 1
 
-        # Phase 2: Validate the constructed string against the LCP matrix
-        # We iterate backwards to use the DP relationship: lcp[i][j] = 1 + lcp[i+1][j+1]
+        for i in range(n):
+            if res[i] > 0:
+                continue
+            if char_val > 26:
+                return ""
+            for j in range(i, n):
+                if lcp[i][j] > 0:
+                    res[j] = char_val
+            char_val += 1
+
+        word = "".join(chr(ord('a') + v - 1) for v in res)
+
         for i in range(n - 1, -1, -1):
             for j in range(n - 1, -1, -1):
-                if s[i] == s[j]:
-                    expected_val = 1
-                    if i + 1 < n and j + 1 < n:
-                        expected_val += lcp[i+1][j+1]
-                    
-                    if lcp[i][j] != expected_val:
-                        return ""
-                else:
-                    if lcp[i][j] != 0:
-                        return ""
-                        
-        return "".join(s)
+                actual_lcp = 0
+                if word[i] == word[j]:
+                    actual_lcp = lcp[i + 1][j + 1] + 1 if i + 1 < n and j + 1 < n else 1
+
+                if lcp[i][j] != actual_lcp:
+                    return ""
+
+        return word
