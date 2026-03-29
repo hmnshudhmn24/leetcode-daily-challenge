e1f42c0c0ef37ba158beae710bfd8b1c462e0a48
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-03-29 09:04:10 +0530

    Add files via upload

---
+++ 2839. Check if Strings Can be Made Equal With Operations I.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        return sorted(s1[0::2]) == sorted(s2[0::2]) and sorted(s1[1::2]) == sorted(s2[1::2])
+
+
+# Example usage
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.canBeEqual("abcd", "cdab"))  # True
