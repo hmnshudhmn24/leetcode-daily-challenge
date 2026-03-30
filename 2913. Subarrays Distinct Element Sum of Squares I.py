341ac116c78350854b0b2bae09d28ac9a74fd67d
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-03-30 19:20:49 +0530

    Add files via upload

---
+++ 2913. Subarrays Distinct Element Sum of Squares I.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumCounts(self, nums: list[int]) -> int:
+        ans = 0
+        n = len(nums)
+        for i in range(n):
+            seen = set()
+            for j in range(i, n):
+                seen.add(nums[j])
+                ans += len(seen) ** 2
+        return ans
