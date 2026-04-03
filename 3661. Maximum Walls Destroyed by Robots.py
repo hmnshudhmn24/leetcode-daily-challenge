df35380eb4d5406ed54f8fffc8959cc870db7b8c
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-04-03 08:43:10 +0530

    Add files via upload

---
+++ 3661. Maximum Walls Destroyed by Robots.py
@@ -0,0 +1,39 @@
+import bisect
+from typing import List
+from functools import cache
+
+class Solution:
+    def maxWalls(self, robots: List[int], distance: List[int], walls: List[int]) -> int:
+        n = len(robots)
+        arr = sorted(zip(robots, distance))
+        walls.sort()
+
+        @cache
+        def dfs(i: int, j: int) -> int:
+            if i < 0:
+                return 0
+
+            left = arr[i][0] - arr[i][1]
+            if i > 0:
+                left = max(left, arr[i - 1][0] + 1)
+
+            l = bisect.bisect_left(walls, left)
+            r = bisect.bisect_left(walls, arr[i][0] + 1)
+            ans = dfs(i - 1, 0) + r - l
+
+            right = arr[i][0] + arr[i][1]
+            if i + 1 < n:
+                if j == 0:
+                    right = min(right, arr[i + 1][0] - arr[i + 1][1] - 1)
+                else:
+                    right = min(right, arr[i + 1][0] - 1)
+
+            l_right = bisect.bisect_left(walls, arr[i][0])
+            r_right = bisect.bisect_left(walls, right + 1)
+            ans = max(ans, dfs(i - 1, 1) + r_right - l_right)
+
+            return ans
+
+        res = dfs(n - 1, 1)
+        dfs.cache_clear()
+        return res
