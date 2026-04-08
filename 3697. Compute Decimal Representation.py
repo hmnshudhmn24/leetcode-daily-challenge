45189ad6c3317b6ea559dbb42e8b7f53afa99f88
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-04-08 08:59:43 +0530

    Add files via upload

---
+++ 3697. Compute Decimal Representation.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def decimalRepresentation(self, n: int) -> List[int]:
+        ans = []
+        p = 1
+        while n:
+            n, v = divmod(n, 10)
+            if v:
+                ans.append(p * v)
+            p *= 10
+        return ans[::-1]
