03784afa637e540229abbe9e72c9b591c54f4734
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-04-15 22:35:26 +0530

    Add files via upload

---
+++ 1507. Reformat Date.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        parts = date.split()
+        months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", 
+                  "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+
+        day = parts[0][:-2].zfill(2)
+        month = months[parts[1]]
+        year = parts[2]
+
+        return f"{year}-{month}-{day}"
