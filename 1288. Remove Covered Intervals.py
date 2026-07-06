d8b5e47c2f6f27bc44cbc303c1fb3e2b6b39b566
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-07-06 12:47:09 +0530

    Add files via upload

---
+++ 1288. Remove Covered Intervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeCoveredIntervals(self, intervals: list[list[int]]) -> int:
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+        ans = 0
+        max_end = -1
+        for start, end in intervals:
+            if end > max_end:
+                ans += 1
+                max_end = end
+        return ans
