90e2dd9aec3f29e43145de2b4c74122f97c9a83b
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-07-13 21:55:41 +0530

    Add files via upload

---
+++ 1291. Sequential Digits.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        res = []
+        for i in range(1, 10):
+            num = i
+            for j in range(i + 1, 10):
+                num = num * 10 + j
+                if low <= num <= high:
+                    res.append(num)
+        return sorted(res)
