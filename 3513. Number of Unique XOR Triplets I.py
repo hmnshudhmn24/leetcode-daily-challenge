5d9359512bc7d8110ce6f25745a80fa6e83a3009
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-07-23 21:50:49 +0530

    Add files via upload

---
+++ 3513. Number of Unique XOR Triplets I.py
@@ -0,0 +1,10 @@
+import math
+
+class Solution:
+    def uniqueXorTriplets(self, nums: list[int]) -> int:
+        n = len(nums)
+
+        if n < 3:
+            return n
+
+        return 1 << n.bit_length()
