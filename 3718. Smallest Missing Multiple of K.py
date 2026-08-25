0ff57ddf39c0e7185e23d88294ff42796796061d
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-08-25 19:23:42 +0530

    Add files via upload

--- 3718. Smallest Missing Multiple of K.py
+++ 3718. Smallest Missing Multiple of K.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class Solution:
-    def smallestMissingMultiple(self, nums: List[int], k: int) -> int:
-        s = set(nums)
-        m = k
-        while m in s:
-            m += k
-        return m
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        seen = set(nums)
+        multiple = k
+        while multiple in seen:
+            multiple += k
+        return multiple
