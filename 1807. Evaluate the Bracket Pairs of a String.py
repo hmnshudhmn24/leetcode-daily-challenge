53e3c48ec9398b76f58c1d8f9dedb82d0ee8bd23
Himanshu Dhiman <himanshu.dhiman2023@GMAIL.COM>
2026-09-26 11:13:19 +0530

    Add files via upload

---
+++ 1807. Evaluate the Bracket Pairs of a String.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
+        d = dict(knowledge)
+        parts = s.split('(')
+        res = [parts[0]]
+        
+        for part in parts[1:]:
+            key, rest = part.split(')')
+            res.append(d.get(key, '?'))
+            res.append(rest)
+            
+        return "".join(res)
